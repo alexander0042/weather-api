233cefc8c148d030a6f65b5a26fe3fb774e02542
Unknown <alexander.rey@queensu.ca>
2021-05-04 15:29:32 -0400

    Update Key

---
+++ keys.js
@@ -0,0 +1,10 @@
+/** 
+ *  Get your API keys here
+ *  https://darksky.net/dev/
+ *  https://developers.google.com/maps/documentation/javascript/get-api-key
+ */
+
+module.exports = {
+  darksky: 'AIzaSyDVgzAMACiyxjBu3Oq5e_kZWbFRwcdOkuI',
+  google: 'AIzaSyDVgzAMACiyxjBu3Oq5e_kZWbFRwcdOkuI'
+}
